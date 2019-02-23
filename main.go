025d68ca7a07d6765671d384d19aff6d689c6465
anthony-y <anthonyybayn@gmail.com>
2019-02-23 16:12:58 +0000

    More error checking, moved showUsage

--- main.go
+++ main.go
@@ -19,49 +19,6 @@ type config struct {
 	AlbumPrefix string `json:"album_prefix"`
 }
 
-func showUsage() {
-	fmt.Println()
-
-	fmt.Println("'scribe' gets and applies meta-data from Deezer to MP3 files.")
-
-	fmt.Println("\nUsage:")
-	fmt.Println("\tscribe.exe [path to album]")
-	fmt.Println("\t(Ensure your folders are named after the albums they contain!)")
-
-	fmt.Println()
-}
-
-func formatAlbumData(album *deezerAlbum) {
-	// Change to release year instead of release date
-	album.Released = album.Released[:4]
-
-	// Make artist title case (sometimes Deezer stores artists in all upper or lower case)
-	album.Artist.Name = strings.Title(strings.ToLower(album.Artist.Name))
-}
-
-// Write meta-data to an mp3 file
-func writeTagMP3(fileName string, trackNum int, album *deezerAlbum) {
-	mp3, err := id3v2.Open(fileName, id3v2.Options{
-		Parse: false,
-	})
-	defer mp3.Close()
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	trackName := album.Tracks.Data[trackNum].Title
-
-	mp3.SetTitle(trackName)
-	mp3.SetAlbum(album.Title)
-	mp3.SetArtist(album.Artist.Name)
-	mp3.SetYear(album.Released)
-	mp3.SetGenre(album.Genres.Data[0].Name)
-
-	mp3.Save()
-}
-
 // Load scribe config file
 func loadConfig() config {
 	configFile := "scribe_config.json"
@@ -99,6 +56,37 @@ func loadConfig() config {
 	return config
 }
 
+func formatAlbumData(album *deezerAlbum) {
+	// Change to release year instead of release date
+	album.Released = album.Released[:4]
+
+	// Make artist title case (sometimes Deezer stores artists in all upper or lower case)
+	album.Artist.Name = strings.Title(strings.ToLower(album.Artist.Name))
+}
+
+// Write meta-data to an mp3 file
+func writeTagMP3(fileName string, trackNum int, album *deezerAlbum) {
+	mp3, err := id3v2.Open(fileName, id3v2.Options{
+		Parse: false,
+	})
+	defer mp3.Close()
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	trackName := album.Tracks.Data[trackNum].Title
+
+	mp3.SetTitle(trackName)
+	mp3.SetAlbum(album.Title)
+	mp3.SetArtist(album.Artist.Name)
+	mp3.SetYear(album.Released)
+	mp3.SetGenre(album.Genres.Data[0].Name)
+
+	mp3.Save()
+}
+
 func main() {
 	// Check args and show usage
 	if len(os.Args) < 2 {
@@ -130,17 +118,14 @@ func main() {
 
 	formatAlbumData(album)
 
-	fmt.Println("Album name: " + album.Title)
-	fmt.Println("Artist: " + album.Artist.Name)
-	fmt.Println("Released: " + album.Released)
-
-	for i, track := range album.Tracks.Data {
-		fmt.Println(strconv.Itoa(i) + ". " + track.Title)
-	}
-
 	albumDir, err := ioutil.ReadDir(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
+		return
+	}
+
+	if len(albumDir) != len(album.Tracks.Data) {
+		log.Fatal("No Deezer results for " + albumName)
 		return
 	}
 
@@ -163,4 +148,16 @@ func main() {
 			return
 		}
 	}
+}
+
+func showUsage() {
+	fmt.Println()
+
+	fmt.Println("'scribe' gets and applies meta-data from Deezer to MP3 files.")
+
+	fmt.Println("\nUsage:")
+	fmt.Println("\tscribe.exe [path to album]")
+	fmt.Println("\t(Ensure your folders are named after the albums they contain!)")
+
+	fmt.Println()
 }
