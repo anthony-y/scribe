acfae8b46ac4479c41ccc04952c2b1626c7d6352
anthony-y <anthonyybayn@gmail.com>
2019-02-23 13:20:37 +0000

    Added config file, refactored tagging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"net/http"
 	"path/filepath"
 	"io/ioutil"
@@ -9,9 +10,99 @@ import (
 	"strconv"
 	"time"
 	"log"
+	"encoding/json"
 
 	"github.com/bogem/id3v2"
 )
+
+type config struct {
+	AlbumPrefix string `json:"album_prefix"`
+}
+
+func showUsage() {
+	fmt.Println()
+
+	fmt.Println("'scribe' gets and applies meta-data from Deezer to MP3 files.")
+
+	fmt.Println("\nUsage:")
+	fmt.Println("\tscribe.exe [path to album]")
+	fmt.Println("\t(Ensure your folders are named after the albums they contain!)")
+
+	fmt.Println()
+}
+
+func formatAlbumData(album *deezerAlbum) {
+	// Change to release year instead of release date
+	album.Released = album.Released[:4]
+
+	// Make artist title case (sometimes Deezer stores artists in all upper or lower case)
+	album.Artist.Name = strings.Title(strings.ToLower(album.Artist.Name))
+}
+
+// Write meta-data to an mp3 file
+func writeTagMP3(fileName string, trackNum int, album *deezerAlbum) error {
+	mp3, err := id3v2.Open(fileName, id3v2.Options{
+		Parse: false,
+	})
+	defer mp3.Close()
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	trackName := album.Tracks.Data[trackNum].Title
+
+	mp3.SetTitle(trackName)
+	mp3.SetAlbum(album.Title)
+	mp3.SetArtist(album.Artist.Name)
+	mp3.SetYear(album.Released)
+	mp3.SetGenre(album.Genres.Data[0].Name)
+
+	mp3.Save()
+
+	return nil
+}
+
+// Load scribe config file
+func loadConfig() config {
+	const defaultConfig = `{
+	"album_prefix": ""
+}`
+
+	configFile := "scribe_config.json"
+
+	// If the config file doesn't exist, make one and write a default config to it
+	if _, err := os.Stat(configFile); os.IsNotExist(err) {
+		newFile, err := os.Create(configFile)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		defer newFile.Close()
+
+		_, err = newFile.Write([]byte(defaultConfig))
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
+
+	configText, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatal("Failed to load config file: ", err)
+		os.Exit(1)
+	}
+
+	var config config
+	err = json.Unmarshal(configText, &config)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	return config
+}
 
 func main() {
 	// Check args and show usage
@@ -20,23 +111,29 @@ func main() {
 		return
 	}
 
-	albumName := filepath.Base(os.Args[1])
+	config := loadConfig()
+
+	albumName := strings.TrimPrefix(filepath.Base(os.Args[1]), config.AlbumPrefix)
+
+	fmt.Println("\n\tWorking...\n")
 
 	client := &http.Client{
 		Timeout: 20 * time.Second,
 	}
 
-	fmt.Println("\n\tWorking...\n")
-
 	albumID, err := searchAlbum(client, albumName)
 	if err != nil {
+		log.Fatal(err)
 		return
 	}
 
 	album, err := getAlbum(client, strconv.Itoa(albumID))
 	if err != nil {
+		log.Fatal(err)
 		return
 	}
+
+	formatAlbumData(album)
 
 	fmt.Println("Album name: " + album.Title)
 	fmt.Println("Artist: " + album.Artist.Name)
@@ -46,17 +143,10 @@ func main() {
 		fmt.Println(strconv.Itoa(i) + ". " + track.Title)
 	}
 
-	err = writeTags(album)
-	if err != nil {
-		return
-	}
-}
-
-func writeTags(album DeezerAlbum) error {
 	albumDir, err := ioutil.ReadDir(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
-		return err
+		return
 	}
 
 	for i, file := range albumDir {
@@ -65,40 +155,17 @@ func writeTags(album DeezerAlbum) error {
 		}
 
 		fileName := filepath.Join(os.Args[1], file.Name())
-		fmt.Println(fileName)
+		fileExt  := filepath.Ext(fileName)
 
-		mp3, err := id3v2.Open(fileName, id3v2.Options{
-			Parse: false,
-		})
-		defer mp3.Close()
-
-		if err != nil {
-			log.Fatal(err)
-			return err
+		if fileExt == ".mp3" {
+			err = writeTagMP3(fileName, i, album)
+		} else {
+			log.Fatal(fileExt + " not supported right now.")
+			return
 		}
 
-		trackName := album.Tracks.Data[i].Title
-
-		mp3.SetTitle(trackName)
-		mp3.SetAlbum(album.Title)
-		mp3.SetArtist(album.Artist.Name)
-		mp3.SetYear(album.Released)
-		mp3.SetGenre(album.Genres.Data[0].Name)
-
-		mp3.Save()
+		if err != nil {
+			return
+		}
 	}
-
-	return nil
-}
-
-func showUsage() {
-	fmt.Println()
-
-	fmt.Println("'scribe' gets and applies meta-data for MP3 files from Genius.")
-
-	fmt.Println("\nUsage:")
-	fmt.Println("\tscribe.exe [path to album]")
-	fmt.Println("\t(Ensure your folders are named after the albums they contain!)")
-
-	fmt.Println()
 }
--- requests.go
+++ requests.go
@@ -5,19 +5,21 @@ import (
 	"net/http"
 	"net/url"
 	"encoding/json"
-	"log"
+	"errors"
 )
 
 // Shoutouts @mholt on GitHub for json-to-go
 // that shit saved my life fr
 
-type DeezerAlbumSearchResult struct {
+// Representation of a Deezer search result
+type deezerAlbumSearchResult struct {
 	Data []struct {
 		ID int `json:"id"`
 	} `json:"data"`
 }
 
-type DeezerAlbum struct {
+// Representation of a Deezer album
+type deezerAlbum struct {
 	Title    string `json:"title"`
 	Link     string `json:"link"`
 	Cover    string `json:"cover"`
@@ -45,68 +47,64 @@ type DeezerAlbum struct {
 func requestDeezerAPI(client *http.Client, path string) ([]byte, error)  {
 	request, err := http.NewRequest("GET", "http://api.deezer.com" + path, nil)
 	if err != nil {
-		log.Fatal(err)
-		return nil, err 
+		
+		return nil, err
 	}
 	request.Header.Set("Content-Type", "application/json")
 
 	response, err := client.Do(request)
 	if err != nil {
-		log.Fatal(err)
 		return nil, err
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
 		return nil, err
 	}
 
 	return body, nil
 }
 
+// Searches Deezer for an album and returns it's ID
 func searchAlbum(client *http.Client, name string) (int, error) {
 	escapedName := url.QueryEscape(name)
 
 	body, err := requestDeezerAPI(client, "/search/album?q=" + escapedName)
 	if err != nil {
-		log.Fatal(err)
 		return -1, err
 	}
 
-	var search DeezerAlbumSearchResult
+	var search deezerAlbumSearchResult
 	err = json.Unmarshal(body, &search)
 	if err != nil {
-		log.Fatal(err)
 		return -1, err
 	}
 
 	if len(search.Data) < 1 {
-		log.Fatal("No results for " + name)
+		err = errors.New("No Deezer results for " + name)
 		return -1, err
 	}
 
 	return search.Data[0].ID, nil
 }
 
-func getAlbum(client *http.Client, ID string) (DeezerAlbum, error) {
-	var album DeezerAlbum
+/*
+	Get a specific album, a search doesn't return all necessary
+	meta-data, so you have to request the actual album
+*/
+func getAlbum(client *http.Client, ID string) (*deezerAlbum, error) {
+	album := &deezerAlbum{}
 
 	body, err := requestDeezerAPI(client, "/album/" + ID)
 	if err != nil {
-		log.Fatal(err)
 		return album, err
 	}
 
 	err = json.Unmarshal(body, &album)
 	if err != nil {
-		log.Fatal(err)
 		return album, err
 	}
-
-	// Change to release year
-	album.Released = album.Released[:4]
 
 	return album, nil
 }
